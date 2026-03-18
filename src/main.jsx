63d7206590daece6cf824887b2d97ffc8bf6c0f9
bintiberamib <bintiberamib@wisc.edu>
2026-03-18 15:54:42 -0500

    Removed redundant router

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.jsx'
-import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <StrictMode>
     <App />
-  </HashRouter>
-
+  </StrictMode>
 )
